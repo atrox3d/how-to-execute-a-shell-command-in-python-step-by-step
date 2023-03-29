48caa537b387b88e362eed7dbe1bf4e1cd9ebdf3
atrox3d <atrox3d@gmail.com>
2023-03-29 23:27:59 +0200

    add subprocess_run_pipe.py

---
+++ linux/subprocess_run_pipe.py
@@ -0,0 +1,28 @@
+import subprocess
+import shlex
+import platform
+
+
+def run_command(command):
+    tokenized = shlex.split(command)
+    print(f'command   | {tokenized}')
+
+    completed = subprocess.run(
+        tokenized,
+        # capture_output=True,        # save output to completed.stdout and/or completed.stderr
+        stdout=subprocess.PIPE,     # capture stdout
+        # stderr=subprocess.PIPE,    # capture stderr
+        stderr=subprocess.STDOUT,   # redirect stderr to stdout
+        text=True                   # save as text, not binary
+    )
+
+    # completed is a CompletedProcess object
+    # completed.args      : tokenized command
+    # completed.returncode: exit code
+    # completed.stdout    : output stream
+    # completed.sterr     : error stream
+    for k, v in vars(completed).items():
+        print(f'completed | {k:15} | {v}')
+
+run_command('date +%a')
+run_command('date %a')
