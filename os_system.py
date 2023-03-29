1c36736e7a8532739ca587f4d66f4a2d8e314f1f
atrox3d <atrox3d@gmail.com>
2023-03-29 05:29:31 +0200

    Using OS System to Run a Command in Python

---
+++ os_system.py
@@ -0,0 +1,16 @@
+import os
+import platform
+
+os_name = platform.system().lower()
+
+if os_name == 'windows':
+    cmd = r'"C:\Program Files\Git\bin\bash.exe" -c daet'
+else:
+    cmd = 'date'
+
+print(f'os        | {os_name}')
+print(f'command   | {cmd}')
+
+exit_code = os.system(cmd)
+
+print(f'exit code | {exit_code}')
