6cfe4583b0b27e7347f31e272fee3135c61d5b67
atrox3d <atrox3d@gmail.com>
2023-03-29 11:56:48 +0200

    add and test linux/os_popen

---
+++ linux/os_popen.py
@@ -0,0 +1,29 @@
+import os
+
+
+def run_command(command):
+    print()
+    print(f'command                 | {command}')
+
+    # open command but does not execute it
+    output = os.popen(command)
+
+    # print output internals
+    print(f'type(output)            | {type(output)}')              # print class os.wrap_close
+    print(f'output.__dict__         | {output.__dict__}')           # print contents of dict (_stream, _proc)
+    for k, v in output.__dict__.items():                            # print each element (_io.TextIOWrapper, Popen)
+        print(f'output[{k:15}] |  {v}')
+    # print contents of Popen
+    print(f'output._proc.args       | {output._proc.args}')         # prints the command
+    print(f'output._proc.returncode | {output._proc.returncode}')   # returncode is still None
+
+    text = output.read()                                            # runs command and gets output
+    print(f'text                    | {text }')
+
+    exit_code = output.close()                                      # close command and get real exit code
+    print(f'exit_code               | {exit_code}')
+
+    print(f'output._proc.returncode | {output._proc.returncode}')   # 0-127
+
+
+run_command('ping -c 5 localhost')
