840c16786448710143e2d26c85377dbd4aaff4bd
atrox3d <atrox3d@gmail.com>
2023-03-29 07:49:06 +0200

    test bash and windows commands

--- os_system.py
+++ os_system.py
@@ -1,9 +1,10 @@
 import os
+import platform
 
 from util.gitbash import GitBash
 
 
-def run_command(command):
+def run_bash_command(command):
     bash = GitBash()
     cmd = bash.get_command(command)
 
@@ -11,10 +12,31 @@ def run_command(command):
     print(f'os_name   | {bash.os}')
     print(f'command   | {cmd}')
 
+    """
+    using os.system() we cannot save the output of
+    the command but we can save its exit code
+    """
     exit_code = os.system(cmd)
 
     print(f'exit code | {exit_code}')
 
 
-for command in 'date ls pwd that\'s an error'.split():
-    run_command(command)
+def run_windows_command(command):
+
+    print()
+    print(f'os_name   | {platform.system()}')
+    print(f'command   | {command}')
+
+    exit_code = os.system(command)
+
+    print(f'exit code | {exit_code}')
+
+
+# run some commands using bash or git bash, if installed
+for command in 'date ls pwd error'.split():
+    run_bash_command(command)
+
+# run some windows commands
+if platform.system() == 'Windows':
+    for command in 'dir error'.split():
+        run_windows_command(command)
