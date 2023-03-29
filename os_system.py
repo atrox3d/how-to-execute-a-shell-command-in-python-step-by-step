d18db0b5edd362395131cba8a5f172420733b824
atrox3d <atrox3d@gmail.com>
2023-03-29 05:36:31 +0200

    comment function

--- os_system.py
+++ os_system.py
@@ -2,7 +2,8 @@ import os
 import platform
 
 
-def bash_command(cmd):
+def get_git_bash_command(cmd):
+    """ tries to run command in git bash if under windows """
     os_name = platform.system().lower()
     if os_name == 'windows':
         os_prefix = r'"C:\Program Files\Git\bin\bash.exe" -c '
@@ -14,7 +15,7 @@ def bash_command(cmd):
     return os_name, command
 
 
-os_name, cmd = bash_command('date')
+os_name, cmd = get_git_bash_command('date')
 print(f'os_name   | {os_name}')
 print(f'command   | {cmd}')
 
