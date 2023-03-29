2359644a6873b0fce487d5e1da79372faf10fd33
atrox3d <atrox3d@gmail.com>
2023-03-29 07:37:00 +0200

    add run_command and loop

--- os_system.py
+++ os_system.py
@@ -2,11 +2,19 @@ import os
 
 from util.gitbash import GitBash
 
-bash = GitBash()
-cmd = bash.get_command('date')
-print(f'os_name   | {bash.os}')
-print(f'command   | {cmd}')
 
-exit_code = os.system(cmd)
+def run_command(command):
+    bash = GitBash()
+    cmd = bash.get_command(command)
 
-print(f'exit code | {exit_code}')
+    print()
+    print(f'os_name   | {bash.os}')
+    print(f'command   | {cmd}')
+
+    exit_code = os.system(cmd)
+
+    print(f'exit code | {exit_code}')
+
+
+for command in 'date ls pwd that\'s an error'.split():
+    run_command(command)
