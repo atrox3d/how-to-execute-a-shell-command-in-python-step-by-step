a649d8b12700c52e13ae040fcf964e6fef986aa3
atrox3d <atrox3d@gmail.com>
2023-03-30 01:33:06 +0200

    temp fix to import

--- os/os_system.py
+++ os/os_system.py
@@ -1,14 +1,34 @@
 import os
 import platform
+import sys
 
-from osutils.gitbash import GitBash
-from osutils.os_detect import (
-    is_windows,
-    is_linux,
-    is_macos,
-    os_name
-)
-
+sys.path.append(os.getcwd())    # works only from project root
+try:
+    from osutils.gitbash import GitBash
+    from osutils.os_detect import (
+        is_windows,
+        is_linux,
+        is_macos,
+        os_name
+    )
+except ModuleNotFoundError:
+    print('cannot import osutils.*')
+    PYTHONPATH=os.environ.get('PYTHONPATH')
+    print(f'{PYTHONPATH = }')
+    print(f'{sys.path   = }')
+    print("""
+    SOLUTIONS:
+    
+    1) run with temporary PYTHONPATH set to project root:
+        PYTHONPATH=path/to/ptoject/root python path/script.py
+    
+    2) export PYTHONPATH with project root set
+        export PYTHONPATH=path/to/ptoject/root 
+        python path/script.py
+    
+    3) insert '..', '...' at the beginning of sys.path
+    """)
+    exit(1)
 
 def run_bash_command(command):
     if is_windows():
