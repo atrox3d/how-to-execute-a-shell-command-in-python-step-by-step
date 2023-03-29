87af6a93c46617a270634804f9655c9b5c65742c
atrox3d <atrox3d@gmail.com>
2023-03-29 13:23:40 +0200

    add run_command and error example

--- linux/subprocess_run_capture.py
+++ linux/subprocess_run_capture.py
@@ -3,16 +3,20 @@ import shlex
 import platform
 
 
-command = shlex.split('ping -c 5 localhost')
-print(f'command   | {command}')
+def run_command(command):
+    command = shlex.split(command)
+    print(f'command   | {command}')
 
 
-completed = subprocess.run(
-    command,
-    capture_output=True,        # save output to completed.stdout and completed.stderr
-    text=True                   # save as text, not binary
-)
+    completed = subprocess.run(
+        command,
+        capture_output=True,        # save output to completed.stdout and/or completed.stderr
+        text=True                   # save as text, not binary
+    )
 
-# completed is a CompletedProcess object
-for k, v in vars(completed).items():
-    print(f'completed | {k:15} | {v}')
+    # completed is a CompletedProcess object
+    for k, v in vars(completed).items():
+        print(f'completed | {k:15} | {v}')
+
+run_command('date +%a')
+run_command('date %a')
