ff97bfb3f01ab9d34db5115fe147f54bf56dc61f
atrox3d <atrox3d@gmail.com>
2023-03-29 12:24:26 +0200

    add os-popen-readline.py

---
+++ windows/os-popen-readline.py
@@ -0,0 +1,14 @@
+import os
+
+from gitbash import GitBash
+
+bash = GitBash()
+command = bash.get_command('ping -n 5 localhost')
+process = os.popen(command)
+
+print(f'command   | {command}')
+while line := process.readline():
+    print(f'line      | {line!r}')
+
+exit_code = process.close()
+print(f'exit_code | {exit_code}')
