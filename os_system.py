933ec16e7f4d157818717b017053b1f4ff110498
atrox3d <atrox3d@gmail.com>
2023-03-29 05:34:50 +0200

    add bash_command()

--- os_system.py
+++ os_system.py
@@ -1,13 +1,21 @@
 import os
 import platform
 
-os_name = platform.system().lower()
-if os_name == 'windows':
-    cmd = r'"C:\Program Files\Git\bin\bash.exe" -c daet'
-else:
-    cmd = 'date'
 
-print(f'os        | {os_name}')
+def bash_command(cmd):
+    os_name = platform.system().lower()
+    if os_name == 'windows':
+        os_prefix = r'"C:\Program Files\Git\bin\bash.exe" -c '
+    else:
+        os_prefix = ''
+
+    command = f'{os_prefix}{cmd}'
+
+    return os_name, command
+
+
+os_name, cmd = bash_command('date')
+print(f'os_name   | {os_name}')
 print(f'command   | {cmd}')
 
 exit_code = os.system(cmd)
